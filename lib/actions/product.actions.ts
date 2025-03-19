2925dd4113dc8ab653422def1b1afb6c099f9b69
menri <m.bardhi@wisch4web.de>
2025-03-19 11:16:08 +0100

    adding caching

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,19 +1,22 @@
 'use server';
 
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/prisma/prisma-client';
 import { toPlainObject } from '../utils'
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 //get latest proucts 
 export async function getLatestProducts()  {
-    const prisma = new PrismaClient();
+    
     const data = await prisma.product.findMany({
+        cacheStrategy: {
+            ttl: 60,
+            swr:120
+        },
         take: LATEST_PRODUCTS_LIMIT,
         orderBy:{createdAt:'desc'}
     })
     return toPlainObject(data);
 }
 export async function getProductBySlug(slug: string) { 
-    const prisma = new PrismaClient();
     const data = await prisma.product.findFirst({
         where:{slug:slug}
     })
